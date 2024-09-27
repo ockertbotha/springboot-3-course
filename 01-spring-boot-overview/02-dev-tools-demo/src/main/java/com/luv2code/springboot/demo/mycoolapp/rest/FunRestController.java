9237c090a9e21759700b3187ad841a41d92dc2f9
Ockert Botha <ockertbotha@gmail.com>
2024-09-27 06:11:10 +0100

    Auto reloading with devtools

--- 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -3,7 +3,6 @@ package com.luv2code.springboot.demo.mycoolapp.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class FunRestController {
 
@@ -12,4 +11,17 @@ public class FunRestController {
   public String sayHello() {
       return "Hello World!";
   }
+
+  // expose a new endoint for "workout"
+  @GetMapping("/workout")
+  public String getDailyWorkout() {
+      return "Run a hard 5k!";
+  }
+
+  // expose a new endoint for "fortune"
+  @GetMapping("/fortune")
+  public String getDailyFortune() {
+      return "Today is your lucky day.";
+  }
+  
 }
