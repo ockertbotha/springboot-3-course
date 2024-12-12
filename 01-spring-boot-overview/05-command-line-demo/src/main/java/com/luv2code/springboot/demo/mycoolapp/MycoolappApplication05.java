60c634eb5bf93aed82745aefde13b397804e8084
Ockert Botha <ockertbotha@gmail.com>
2024-12-12 06:58:22 +0000

    Adding 05-command-line-demo

---
+++ 01-spring-boot-overview/05-command-line-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication05.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demo.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappApplication05 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoolappApplication05.class, args);
+	}
+
+}
