5a2bcc6200e1a72befbca5d4e9f211995e93ff6c
Ockert Botha <ockertbotha@gmail.com>
2024-12-14 07:44:35 +0000

    finished  06-custom-properties-demo

--- 01-spring-boot-overview/06-custom-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController06.java
+++ 01-spring-boot-overview/06-custom-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController06.java
@@ -1,10 +1,24 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController06 {
+
+  //inject properties for coach.name and team.name
+  @Value("${coach.name}")
+  private String coachName;
+
+  @Value("${team.name}")
+  private String teamName;
+
+  // expose new enpoint for "teaminfo"
+  @GetMapping("/teaminfo")
+  public String getTeamInfo() {
+      return "Coach: " + coachName + ", Team name: " + teamName;
+  }
 
   // expose "/" that returns "Hello World"
   @GetMapping("/")
