26e8ffb6c8d458252619b5903062d2e9720b87f9
Ockert Botha <ockertbotha@gmail.com>
2024-12-15 08:50:39 +0000

    Adding 01-spring-boot-core and finished it.

---
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/DemoController.java
@@ -0,0 +1,21 @@
+package com.luv2code.springcoredemo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class DemoController {
+
+  // define a private field for the dependency
+  private Coach myCoach;
+
+  // define a constructor for dependency inhection
+  public DemoController(Coach theCoach){
+    myCoach = theCoach;
+  }
+
+  @GetMapping("/dailyworkout")
+  public String getDailyWorkout() {
+      return myCoach.getDailyWorkout();
+  }
+}
