a53585e1489d8c96b91586180501e50078be8a20
Ockert Botha <ockertbotha@gmail.com>
2024-12-18 05:57:50 +0000

    Adding 07-bean-scopes

---
+++ 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/luv2code/springcoredemo07/common/Coach.java
@@ -0,0 +1,6 @@
+package com.luv2code.springcoredemo07.common;
+
+public interface Coach {
+
+  String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/luv2code/springcoredemo07/rest/DemoController.java
@@ -0,0 +1,25 @@
+package com.luv2code.springcoredemo07.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springcoredemo07.common.Coach;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class DemoController {
+
+  // define a private field for the dependency
+  private Coach myCoach;
+
+  public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+    System.out.println("In constructor: " + getClass().getSimpleName());
+    myCoach = theCoach;  
+  }
+
+  @GetMapping("/dailyworkout")
+  public String getDailyWorkout() {
+      return myCoach.getDailyWorkout();
+  }
+}
