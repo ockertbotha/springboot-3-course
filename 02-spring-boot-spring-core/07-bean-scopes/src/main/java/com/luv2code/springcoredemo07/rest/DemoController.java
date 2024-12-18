f244b3b2e7d6777ddf1dab39127441ab55a98fd2
Ockert Botha <ockertbotha@gmail.com>
2024-12-18 06:17:13 +0000

    Finished 07-bean-scopes

--- 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/luv2code/springcoredemo07/rest/DemoController.java
+++ 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/luv2code/springcoredemo07/rest/DemoController.java
@@ -6,20 +6,33 @@ import com.luv2code.springcoredemo07.common.Coach;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class DemoController {
 
   // define a private field for the dependency
   private Coach myCoach;
+  private Coach anotherCoach;
 
-  public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+  public DemoController(
+              @Qualifier("cricketCoach") Coach theCoach,
+              @Qualifier("cricketCoach") Coach theAnotherCoach) {
+
     System.out.println("In constructor: " + getClass().getSimpleName());
-    myCoach = theCoach;  
+
+    myCoach = theCoach;
+    anotherCoach = theAnotherCoach;  
   }
 
   @GetMapping("/dailyworkout")
   public String getDailyWorkout() {
       return myCoach.getDailyWorkout();
+  }
+
+  @GetMapping("/check")
+  public String check() {
+      return "Comparing beans: myCoash == anotherCoach, " + (myCoach == anotherCoach);
   }
 }
