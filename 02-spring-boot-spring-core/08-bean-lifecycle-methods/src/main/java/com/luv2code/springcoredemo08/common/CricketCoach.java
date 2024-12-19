0d5c108f39dceb6d63451ef23e341d891cbfe5cf
Ockert Botha <ockertbotha@gmail.com>
2024-12-19 06:37:22 +0000

    Finished 08-bean-lifecycle-methods

--- 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo08/common/CricketCoach.java
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo08/common/CricketCoach.java
@@ -2,11 +2,26 @@ package com.luv2code.springcoredemo08.common;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
 public class CricketCoach implements Coach {
 
   public CricketCoach() {
     System.out.println("In constructor: " + getClass().getSimpleName());
+  }
+
+  // define our init method
+  @PostConstruct
+  public void doMyStartupStuff() {
+    System.out.println("Inside method doMyStartupStuff(): "  + getClass().getSimpleName());
+  }
+
+  // define our destroy method
+  @PreDestroy
+  public void doMyCleanupStuff() {
+    System.out.println("Inside method doMyCleanupStuff(): "  + getClass().getSimpleName());
   }
 
   @Override
