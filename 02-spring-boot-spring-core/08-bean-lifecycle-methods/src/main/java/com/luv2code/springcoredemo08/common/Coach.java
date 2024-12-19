7aefccfd76c956fdc47f1dec01f98206ffcf8910
Ockert Botha <ockertbotha@gmail.com>
2024-12-19 06:29:42 +0000

    Adding 08-bean-lifecycle-methods

---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo08/common/Coach.java
@@ -0,0 +1,6 @@
+package com.luv2code.springcoredemo08.common;
+
+public interface Coach {
+
+  String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo08/rest/DemoController.java
@@ -0,0 +1,29 @@
+package com.luv2code.springcoredemo08.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springcoredemo08.common.Coach;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class DemoController {
+
+  // define a private field for the dependency
+  private Coach myCoach;
+
+  public DemoController(
+              @Qualifier("cricketCoach") Coach theCoach) {
+
+    System.out.println("In constructor: " + getClass().getSimpleName());
+    myCoach = theCoach;
+  }
+
+  @GetMapping("/dailyworkout")
+  public String getDailyWorkout() {
+      return myCoach.getDailyWorkout();
+  }
+
+}
