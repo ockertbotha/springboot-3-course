cbaeb895875974d4b64e87eaf910e995a4912532
Ockert Botha <ockertbotha@gmail.com>
2025-01-04 06:33:06 +0000

    Finished Service Layer Implementation

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-demo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-demo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -6,24 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-  // quick and dirty: inject employee dao
-  private EmployeeDAO employeeDAO;
+  private EmployeeService employeeService;
 
-  public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-    employeeDAO = theEmployeeDAO;
+  public EmployeeRestController(EmployeeService theEmployeeService) {
+    employeeService = theEmployeeService;
   }
 
   // expose "/employees" and return list of employees
   @GetMapping("/employees")
   public List<Employee> findAll() {
-    return employeeDAO.findAll();
+    return employeeService.findAll();
   }
   
 }
