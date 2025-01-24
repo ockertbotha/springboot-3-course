c242c030ffd965b9b3711ce4f3f168badb6dfa75
Ockert Botha <ockertbotha@gmail.com>
2025-01-24 07:06:19 +0000

    Finished Spring Boot - Spring MVC Binding Request Params

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.thymeleafdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -24,7 +25,6 @@ public class HelloWorldController {
 
   // need a controller method to read form data
   // add data to the model
-
   @RequestMapping("/processFormV2")
   public String letsShoutDude(HttpServletRequest request, Model model) {
     
@@ -36,6 +36,21 @@ public class HelloWorldController {
 
     // create the message
     String result = "Yo! " + theName;
+
+    // add message to the model
+    model.addAttribute("message", result);
+
+    return "helloworld";
+  }
+
+  @RequestMapping("/processFormV3")
+  public String processFormV3(@RequestParam("studentName") String theName, Model model) {
+    
+    // convert the data to all caps
+    theName = theName.toUpperCase();
+
+    // create the message
+    String result = "Yo from V3! " + theName;
 
     // add message to the model
     model.addAttribute("message", result);
