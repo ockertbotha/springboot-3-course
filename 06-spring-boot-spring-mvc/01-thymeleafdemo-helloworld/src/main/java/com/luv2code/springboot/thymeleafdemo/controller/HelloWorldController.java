92da224568e8d0e6f87d99c8db329d5a7e3a505b
Ockert Botha <ockertbotha@gmail.com>
2025-01-29 06:04:37 +0000

    Finished Spring Boot - GetMapping and PostMapping

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -2,6 +2,8 @@ package com.luv2code.springboot.thymeleafdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,7 +14,7 @@ import jakarta.servlet.http.HttpServletRequest;
 public class HelloWorldController {
 
   // new controller method to show the initial HTML form
-  @RequestMapping("/showForm")
+  @GetMapping("/showForm")
   public String requestMethodName() {
     return "helloworld-form";
   }
@@ -43,7 +45,7 @@ public class HelloWorldController {
     return "helloworld";
   }
 
-  @RequestMapping("/processFormV3")
+  @PostMapping("/processFormV3")
   public String processFormV3(@RequestParam("studentName") String theName, Model model) {
     
     // convert the data to all caps
