b857a2759c1f98eb935ef7583f1e2dc6fdb39118
Ockert Botha <ockertbotha@gmail.com>
2025-02-25 06:44:39 +0000

    Finished Spring Boot - Spring MVC Form Data Binding - Drop Down Lists

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -3,7 +3,8 @@ package com.luv2code.springboot.thymeleafdemo.model;
 public class Student {
 
   private String firstName;
-  private String lastName;  
+  private String lastName;
+  private String country;  
 
   public Student() {
   }
@@ -23,4 +24,13 @@ public class Student {
   public void setLastName(String lastName) {
     this.lastName = lastName;
   }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+  
 }
