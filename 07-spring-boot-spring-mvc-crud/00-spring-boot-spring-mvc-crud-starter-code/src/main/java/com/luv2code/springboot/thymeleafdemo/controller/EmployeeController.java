ccd3e8dd4a6c9436264851fddbb6cac1b4aac7b4
Ockert Botha <ockertbotha@gmail.com>
2025-05-03 05:48:03 +0100

    Spring Boot - Spring MVC CRUD - Delete Employee Completed

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -64,6 +64,15 @@ public class EmployeeController {
     return "employees/employee-form"; 
   }
 
+  @GetMapping("/delete")
+  public String delete(@RequestParam("employeeId") int theId) {
+    
+    // delete the employee
+    employeeService.deleteById(theId);
+    
+    // redirect to /employees/list
+    return "redirect:/employees/list";
+  }
 
 
   @PostMapping("/save")
