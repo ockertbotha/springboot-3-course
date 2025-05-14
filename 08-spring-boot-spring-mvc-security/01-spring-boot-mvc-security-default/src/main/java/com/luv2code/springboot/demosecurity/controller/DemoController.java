92f6d0db7aefe5b0d9d992b88923039f7241a221
Ockert Botha <ockertbotha@gmail.com>
2025-05-14 08:11:35 +0100

    Spring Boot - Spring MVC Security - Restrict URLs Based on Roles - Complete

--- 08-spring-boot-spring-mvc-security/01-spring-boot-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
@@ -8,7 +8,22 @@ public class DemoController {
 
   @GetMapping("/")
   public String showHome() {
-      return "home";
+      
+    return "home";
+  }
+
+  // add a request mapping for /leaders
+  @GetMapping("/leaders")
+  public String showLeaders() {
+      
+    return "leaders";
+  }
+
+  // add a request mapping for /systems
+  @GetMapping("/systems")
+  public String showSystems() {
+      
+    return "systems";
   }
 
 }
