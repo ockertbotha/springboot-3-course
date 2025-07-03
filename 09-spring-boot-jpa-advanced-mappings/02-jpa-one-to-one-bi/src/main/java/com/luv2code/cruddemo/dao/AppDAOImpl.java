7bb45af5f10488432b81068958829f4d7c536240
Ockert Botha <ockertbotha@gmail.com>
2025-07-03 09:50:13 +0100

    Spring Boot - JPA Advanced - Bi-Directional - Find - Complete

---
+++ 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,54 @@
+package com.luv2code.cruddemo.dao;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
+
+import jakarta.persistence.EntityManager;
+
+@Repository
+public class AppDAOImpl implements AppDAO {
+
+    // define the field for the entity manager
+    private EntityManager entityManager;
+
+    
+    // inject entity manager using constructor injection
+    public AppDAOImpl(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+    
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Instructor findInstructorById(int theId) {
+        // find the instructor by id
+        return entityManager.find(Instructor.class, theId);
+    }
+
+    @Override  
+    @Transactional
+    public void deleteInstructorById(int theId) {
+        // find the instructor by id
+        Instructor tempInstructor = entityManager.find(Instructor.class, theId);
+        // delete the instructor
+        if (tempInstructor != null) {
+            entityManager.remove(tempInstructor);
+        }
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public InstructorDetail findInstructorDetailById(int theId) {
+        // find the instructor detail by id
+        return entityManager.find(InstructorDetail.class, theId); 
+    }
+
+}
